c826ea672898e079e0ceea771f1998073e710733
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-01 13:54:47 -0500

    Build it visibility challenge.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,38 +1,101 @@
 'use strict';
 
-var data = {
-  title: 'Visibility Toggle',
-  showDetails: false
-  // buttonActive: false
+// babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+// end result @ indecision.mead.io
+// - dom elements - https://reactjs.org/docs/dom-elements.html
+// - synthetic events - https://reactjs.org/docs/events.html
+
+var app = {
+  title: 'App title',
+  subtitle: 'It even has a subtitle!',
+  options: []
 };
 
-var toggleVisibility = function toggleVisibility() {
-  data.showDetails = !data.showDetails;
-  render();
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  // console.log('form submitted')
+  // elements contains a list of all the elements and are index by their name attribute
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = ''; // clear the input field
+    renderIndecisionApp();
+  }
+  console.log(app.options);
 };
 
-var render = function render() {
-  var jsx = React.createElement(
+var onMakeDecision = function onMakeDecision() {
+  // generate a random number among the array numbers so you can pull the item out by its index
+  var randomNum = Math.floor(Math.random() * app.options.length);
+  // console.log(randomNum)
+  // console.log(app.options[randomNum])
+  var option = app.options[randomNum];
+  console.log(option);
+};
+
+var onRemoveAll = function onRemoveAll() {
+  if (app.options.length > 0) {
+    app.options = [];
+    renderIndecisionApp();
+  }
+};
+
+var appRoot = document.getElementById('app');
+
+var renderIndecisionApp = function renderIndecisionApp() {
+  var template = React.createElement(
     'div',
     null,
     React.createElement(
       'h1',
       null,
-      data.title
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
       'button',
-      { onClick: toggleVisibility },
-      data.showDetails ? 'Hide Details' : 'Show Details'
+      { disabled: app.options.length === 0, onClick: onMakeDecision },
+      'What should I do?'
     ),
-    data.showDetails && React.createElement(
-      'p',
+    app.options.length > 0 ? React.createElement(
+      'button',
+      { onClick: onRemoveAll },
+      'Remove All'
+    ) : '',
+    React.createElement(
+      'ol',
       null,
-      'Hey, you can see details now!'
+      app.options.map(function (option) {
+        return React.createElement(
+          'li',
+          { key: option },
+          option
+        );
+      })
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
     )
   );
 
-  ReactDOM.render(jsx, document.getElementById('app'));
+  ReactDOM.render(template, appRoot);
 };
 
-render();
+renderIndecisionApp();
--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,3 +1,5 @@
+// babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
 const data = {
   title: 'Visibility Toggle',
   showDetails: false
