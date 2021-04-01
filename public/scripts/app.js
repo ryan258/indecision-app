b4f748f6ef0c87f88c67892a7597c0ef0b3e5620
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-01 12:07:31 -0500

    Apply user inputs + forms

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,101 @@
 'use strict';
 
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+// end result @ indecision.mead.io
+// - dom elements - https://reactjs.org/docs/dom-elements.html
+// - synthetic events - https://reactjs.org/docs/events.html
 
 var app = {
   title: 'App title',
   subtitle: 'It even has a subtitle!',
-  options: ['option 1', 'option 2', 'option 3']
+  // options: ['option 1', 'option 2', 'option 3']
+  options: []
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options' : 'No options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'one...'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'two'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'three'
-    )
-  )
-);
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  // console.log('form submitted')
+  // elements contains a list of all the elements and are index by their name attribute
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = ''; // clear the input field
+    renderIndecisionApp();
+  }
+  console.log(app.options);
+};
+
+var onRemoveAll = function onRemoveAll() {
+  if (app.options.length > 0) {
+    app.options = [];
+    renderIndecisionApp();
+  }
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderIndecisionApp = function renderIndecisionApp() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    app.options.length > 0 ? React.createElement(
+      'button',
+      { onClick: onRemoveAll },
+      'Remove All'
+    ) : '',
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        'one...'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'two'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'three'
+      )
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+renderIndecisionApp();
--- src/app.js
+++ src/app.js
@@ -1,25 +1,62 @@
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+// end result @ indecision.mead.io
+// - dom elements - https://reactjs.org/docs/dom-elements.html
+// - synthetic events - https://reactjs.org/docs/events.html
 
 const app = {
   title: 'App title',
   subtitle: 'It even has a subtitle!',
-  options: ['option 1', 'option 2', 'option 3']
+  // options: ['option 1', 'option 2', 'option 3']
+  options: []
 }
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+const onFormSubmit = (e) => {
+  e.preventDefault()
+  // console.log('form submitted')
+  // elements contains a list of all the elements and are index by their name attribute
+  const option = e.target.elements.option.value
 
-    <ol>
-      <li>one...</li>
-      <li>two</li>
-      <li>three</li>
-    </ol>
-  </div>
-)
+  if (option) {
+    app.options.push(option)
+    e.target.elements.option.value = '' // clear the input field
+    renderIndecisionApp()
+  }
+  console.log(app.options)
+}
+
+const onRemoveAll = () => {
+  if (app.options.length > 0) {
+    app.options = []
+    renderIndecisionApp()
+  }
+}
 
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+const renderIndecisionApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+
+      {app.options.length > 0 ? <button onClick={onRemoveAll}>Remove All</button> : ''}
+
+      <ol>
+        <li>one...</li>
+        <li>two</li>
+        <li>three</li>
+      </ol>
+      {/* we're going to watch for the whole form to submit */}
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  )
+
+  ReactDOM.render(template, appRoot)
+}
+
+renderIndecisionApp()
