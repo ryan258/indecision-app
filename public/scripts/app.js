96749b85f1175a2ed562afca07dfed4d03479278
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-01 18:24:37 -0500

    Nest components

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,8 +13,35 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 // Component is a class itself that gives us the features of React
 // react components require that a special method -> render
 // - and this returns all the JSX
-var Header = function (_React$Component) {
-  _inherits(Header, _React$Component);
+
+var IndecisionApp = function (_React$Component) {
+  _inherits(IndecisionApp, _React$Component);
+
+  function IndecisionApp() {
+    _classCallCheck(this, IndecisionApp);
+
+    return _possibleConstructorReturn(this, (IndecisionApp.__proto__ || Object.getPrototypeOf(IndecisionApp)).apply(this, arguments));
+  }
+
+  _createClass(IndecisionApp, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(Header, null),
+        React.createElement(Action, null),
+        React.createElement(Options, null),
+        React.createElement(AddOption, null)
+      );
+    }
+  }]);
+
+  return IndecisionApp;
+}(React.Component);
+
+var Header = function (_React$Component2) {
+  _inherits(Header, _React$Component2);
 
   function Header() {
     _classCallCheck(this, Header);
@@ -45,8 +72,8 @@ var Header = function (_React$Component) {
   return Header;
 }(React.Component);
 
-var Action = function (_React$Component2) {
-  _inherits(Action, _React$Component2);
+var Action = function (_React$Component3) {
+  _inherits(Action, _React$Component3);
 
   function Action() {
     _classCallCheck(this, Action);
@@ -72,8 +99,8 @@ var Action = function (_React$Component2) {
   return Action;
 }(React.Component);
 
-var Options = function (_React$Component3) {
-  _inherits(Options, _React$Component3);
+var Options = function (_React$Component4) {
+  _inherits(Options, _React$Component4);
 
   function Options() {
     _classCallCheck(this, Options);
@@ -101,7 +128,10 @@ var Options = function (_React$Component3) {
           "li",
           null,
           "here!"
-        )
+        ),
+        React.createElement(Option, null),
+        React.createElement(Option, null),
+        React.createElement(Option, null)
       );
     }
   }]);
@@ -109,8 +139,31 @@ var Options = function (_React$Component3) {
   return Options;
 }(React.Component);
 
-var AddOption = function (_React$Component4) {
-  _inherits(AddOption, _React$Component4);
+var Option = function (_React$Component5) {
+  _inherits(Option, _React$Component5);
+
+  function Option() {
+    _classCallCheck(this, Option);
+
+    return _possibleConstructorReturn(this, (Option.__proto__ || Object.getPrototypeOf(Option)).apply(this, arguments));
+  }
+
+  _createClass(Option, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "li",
+        null,
+        "Option component"
+      );
+    }
+  }]);
+
+  return Option;
+}(React.Component);
+
+var AddOption = function (_React$Component6) {
+  _inherits(AddOption, _React$Component6);
 
   function AddOption() {
     _classCallCheck(this, AddOption);
@@ -139,14 +192,7 @@ var AddOption = function (_React$Component4) {
   return AddOption;
 }(React.Component);
 
-var jsx = React.createElement(
-  "div",
-  null,
-  React.createElement(Header, null),
-  React.createElement(Action, null),
-  React.createElement(Options, null),
-  React.createElement(AddOption, null)
-);
-
 // to render to the screen we need to use ReactDOM
-ReactDOM.render(jsx, document.getElementById('app'));
+
+
+ReactDOM.render(React.createElement(IndecisionApp, null), document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -3,6 +3,20 @@
 // Component is a class itself that gives us the features of React
 // react components require that a special method -> render
 // - and this returns all the JSX
+
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    )
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -31,8 +45,17 @@ class Options extends React.Component {
         <li>options</li>
         <li>component</li>
         <li>here!</li>
+        <Option />
+        <Option />
+        <Option />
       </ul>
     )
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return <li>Option component</li>
   }
 }
 
@@ -47,14 +70,5 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-)
-
 // to render to the screen we need to use ReactDOM
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
