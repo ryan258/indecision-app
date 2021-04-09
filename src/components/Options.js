306dcd9ddc6cf66dc00b430d504b655aff907f58
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-09 09:46:46 -0500

    Change functional components over to their shorthand form.

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import Option from './Option'
 
-const Options = (props) => {
-  const numberOfOptions = props.options.length
-  return (
-    <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-      {props.options.length === 0 && <p>Please add an option, or else...</p>}
-      {props.options.map((option) => (
-        <Option key={option} optionText={option} handleDeleteOption={props.handleDeleteOption} />
-      ))}
-    </div>
-  )
-}
+const Options = (props) => (
+  <div>
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    {props.options.length === 0 && <p>Please add an option, or else...</p>}
+    {props.options.map((option) => (
+      <Option key={option} optionText={option} handleDeleteOption={props.handleDeleteOption} />
+    ))}
+  </div>
+)
 
 export default Options
