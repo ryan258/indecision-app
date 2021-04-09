e4b55ecc22a722333c3ad1bf7dbf86c1e9040bc9
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-09 13:51:28 -0500

    Add and style Modal window.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,16 @@ const OptionModal = (props) => (
     // close on escape key or clicking bg
     onRequestClose={props.handleClearSelectedOption}
     contentLabel="Selected Option"
+    // close transition speed - time before gutting the div
+    closeTimeoutMS={200}
+    // override built in styles with a class name when we set a className on the modal
+    className="modal"
   >
-    <h3>Selected Option 👻</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>You bet'cha!</button>
+    <h3 className="modal__title">Selected Option 👻</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleClearSelectedOption}>
+      You bet'cha!
+    </button>
   </Modal>
 )
 
